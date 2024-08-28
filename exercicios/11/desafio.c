f2a9453952d42acaa9cd56238387e9747d71cf58
Guilherme Araújo Magalhães <136641459+araujocg@users.noreply.github.com>
2024-08-28 15:04:39 -0400

    linguagem-c 28/08/24

--- exercicios/11/desafio.c
+++ exercicios/11/desafio.c
@@ -3,15 +3,31 @@
 
 struct Complaints{
     int id;
-    char name;
-    char text;
+    char name[30];
+    char text[200];
     bool resolved;
     int level;
 };
 
 
 int main(){
+    
     struct Complaints Complain[50]; 
+    int moreComplaints;
+
+    // while (/* condition */)
+    // {
+    //     /* code */
+    // }
+    
+
+    for (int i = 0; i < 50; i++){
+        printf("Existe mais alguma reclamação? (1 para Sim e 2 para Não):");
+        scanf("%d", &moreComplaints);
+
+        // moreComplaints? return : NULL;
+    }
+    
 
     return 0;
 }
