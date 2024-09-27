6a2cee56945ca1123b7b038baf8c6c3af3e9f782
Guilherme Araújo Magalhães <136641459+araujocg@users.noreply.github.com>
2024-09-26 23:44:32 -0400

    linguagem-c 26/09/24

--- exercicios/desafios/challenge03.c
+++ exercicios/desafios/challenge03.c
@@ -3,35 +3,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 int main(){
 
-    char *word = malloc(100);
-    if(word == NULL){
-        perror("Initial allocation failed");
-        return 1;
-    }
+    char word[50];
+    bool checker = true;
 
     printf("Write the word u want to know if is a Palindrome: ");
-    scanf("%99s", &word); // read a word with at most 99 characters
+    scanf("%s", &word);
 
-    size_t newSize = strlen(word) + 1;
-    char *newWord = realloc(word, newSize);
+    size_t length = strlen(word + 1);
 
-     if(newWord == NULL){
-        perror("Initial allocation failed");
-        return 1;
+    for(int i = 0; i < length; i++){
+        if(word[i] != word[length - i]){
+            printf("End right here: %d\n", i);
+            checker = false;
+            break;
+        }
     }
 
-    word = newWord;
-
-    printf("%s", word);
-
-    for(int i = 0; i < 5; i++){
-
+    if(checker == true){
+        printf("Your word is a Palindrome!!");
+    } else{
+        printf("Your word are not a Palindrome! :(");
     }
+
+    // printf("%d", length);
+    // printf("first letter of word: %c", word[length]);
+
+
+
+
+
+
+
+
+
+    // char *word = malloc(100);
+    // if(word == NULL){
+    //     perror("Initial allocation failed");
+    //     return 1;
+    // }
+
+    // printf("Write the word u want to know if is a Palindrome: ");
+    // scanf("%99s", &word); // read a word with at most 99 characters
+
+    // size_t newSize = strlen(word) + 1;
+    // char *newWord = realloc(word, newSize);
+
+    // if(newWord == NULL){
+    //     perror("Initial allocation failed");
+    //     return 1;
+    // }
+
+    // word = newWord;
+
+    // printf("%s", newWord);
+
+    // for(int i = 0; i < 5; i++){
+
+    // }
     
-    free(word);
+    // free(word);
 
     return 0;
 }
--- exercicios/desafios/tempCodeRunnerFile.c
+++ exercicios/desafios/tempCodeRunnerFile.c
@@ -1,2 +1,1 @@
-
-    printf("Digit your number: ");
+Palindrome
